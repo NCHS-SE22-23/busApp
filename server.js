9c50ad2b948db69e9b0da3d6fff7742c3263336c
arremiasz <arremiasz@stu.naperville203.org>
2023-01-26 15:11:12 -0600

    transitioned to node.js

---
+++ server.js
@@ -0,0 +1,26 @@
+// Load Node modules
+var express = require('express');
+const ejs = require('ejs');
+// Initialise Express
+var app = express();
+// Render static files
+app.use(express.static('public'));
+// Set the view engine to ejs
+app.set('view engine', 'ejs');
+// Port website will run on
+app.listen(8080);
+
+// *** GET Routes - display pages ***
+// Root Route
+app.use(express.static('public'));
+app.get('/', function (req, res) {
+    res.render('pages/index');
+});
+
+app.get('/buslist', function (req, res) {
+    res.render('pages/buslist');
+});
+
+app.get('/logs', function (req, res) {
+    res.render('pages/logs');
+});
