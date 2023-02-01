700bb85cb4f3407a737be1bb7a8245e486a10779
adeshkumar1 <89107712+adeshkumar1@users.noreply.github.com>
2023-02-01 11:44:40 -0600

    idk

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 // Port website will run on
 app.listen(8080);
-console.log('On Successfully (probably)')
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({
@@ -69,19 +68,3 @@ app.get('/getbus', (req, res) => {
     let data = JSON.parse(datajson);
     res.send(datajson);
 })
-
-const {OAuth2Client} = require('google-auth-library');
-const client = new OAuth2Client(CLIENT_ID);
-async function verify() {
-  const ticket = await client.verifyIdToken({
-      idToken: token,
-      audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
-      // Or, if multiple clients access the backend:
-      //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
-  });
-  const payload = ticket.getPayload();
-  const userid = payload['sub'];
-  // If request specified a G Suite domain:
-  // const domain = payload['hd'];
-}
-verify().catch(console.error);
