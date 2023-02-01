2d556575843ebfbf1cd4201695b9ce6e74423ec4
adeshkumar1 <89107712+adeshkumar1@users.noreply.github.com>
2023-02-01 11:23:14 -0600

    oops forgor to commit

--- server.js
+++ server.js
@@ -68,3 +68,19 @@ app.get('/getbus', (req, res) => {
     let data = JSON.parse(datajson);
     res.send(datajson);
 })
+
+const {OAuth2Client} = require('google-auth-library');
+const client = new OAuth2Client(CLIENT_ID);
+async function verify() {
+  const ticket = await client.verifyIdToken({
+      idToken: token,
+      audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+      // Or, if multiple clients access the backend:
+      //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+  });
+  const payload = ticket.getPayload();
+  const userid = payload['sub'];
+  // If request specified a G Suite domain:
+  // const domain = payload['hd'];
+}
+verify().catch(console.error);
