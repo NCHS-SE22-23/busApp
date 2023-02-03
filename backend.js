4ae36a9655e370379cb988114b6c45cf9f4b3834
adeshkumar1 <89107712+adeshkumar1@users.noreply.github.com>
2023-02-03 10:38:35 -0600

    hjk,

---
+++ backend.js
@@ -0,0 +1,10 @@
+function onSignIn(googleUser) {
+    var id_token = googleUser.getAuthResponse().id_token;
+}
+var xhr = new XMLHttpRequest();
+xhr.open('POST', 'https://yourbackend.example.com/tokensignin');
+xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+xhr.onload = function() {
+console.log('Signed in as: ' + xhr.responseText);
+};
+  xhr.send('idtoken=' + id_token);
