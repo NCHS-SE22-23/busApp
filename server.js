629b456c08c62534f6605dc1a4ba0e739bf94bc9
adeshkumar1 <89107712+adeshkumar1@users.noreply.github.com>
2023-02-23 11:50:26 -0600

    Google Sign in functions

--- server.js
+++ server.js
@@ -10,6 +10,23 @@ app.set('view engine', 'ejs');
 // Port website will run on
 app.listen(8080);
 
+const {OAuth2Client} = require('google-auth-library');
+const client = new OAuth2Client(CLIENT_ID);
+
+async function verify() {
+  const ticket = await client.verifyIdToken({
+      idToken: token,
+      audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+      // Or, if multiple clients access the backend:
+      //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+  });
+  const payload = ticket.getPayload();
+  const userid = payload['sub'];
+  // If request specified a G Suite domain:
+  // const domain = payload['hd'];
+}
+
+verify().catch(console.error);
 app.use(express.json({  extended: true }));
 app.use(express.urlencoded({  extended: true }));
 
