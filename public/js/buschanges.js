c78fd8413aa6b9e15f9de5b4e21e69c7ff31995a
arremiasz <arremiasz@stu.naperville203.org>
2023-02-28 11:20:46 -0600

    working on buschanges

--- public/js/buschanges.js
+++ public/js/buschanges.js
@@ -1,15 +1,87 @@
 function viewBusEdit() {
-    let searchTab = document.getElementById('searchBusses');
-    let bussesTab = document.getElementById('viewBusses');
-    searchTab.style.display = "none";
-    bussesTab.style.display = "flex";
+    let viewTab = document.getElementById('viewBusses');
+    let editTab = document.getElementById('editBusses');
+    viewTab.style.display = "none";
+    editTab.style.display = "flex";
 }
 
 function viewBusChanges() {
-    let searchTab = document.getElementById('searchBusses');
-    let bussesTab = document.getElementById('allBusses');
-    searchTab.style.display = "flex";
-    bussesTab.style.display = "none";
+    let viewTab = document.getElementById('viewBusses');
+    let editTab = document.getElementById('editBusses');
+    viewTab.style.display = "flex";
+    editTab.style.display = "none";
+}
+
+function getBusses() {
+    let o = document.getElementsByClassName('busObj');
+    for (let i = 0; i < o.length; i++) {
+        o[i].style.display = 'none';
+    }
+
+    fetch('/getbus')
+    .then(response => { 
+        if(response.ok) {
+            return response.json(); // not important
+        }
+        }).then(data => {
+        if(data) { // if there is data
+            let i = 0;
+            while(i < data.buslist.length) {
+                let div = document.createElement("div");
+                div.classList.add('busObj')
+                div.classList.add('flex-fill');
+
+                div.style.backgroundColor = "rgb(255, 44, 44)";
+                
+
+                div.style.borderRadius = "30px";
+                div.style.margin = "10px";
+
+                var h = window.innerHeight;
+                div.style.height = (h-180)/10+"px";
+
+                let busNumber = data.buslist[i].number;
+                div.textContent = busNumber;
+                div.style.textAlign = 'center';
+                div.style.fontFamily = 'Gill Sans';
+
+                div.onclick = editChange;
+
+                document.getElementById('editBusses').appendChild(div);  
+                i++;
+
+                function editChange() {
+                    
+                }
+            }
+        }
+    }).catch(err => console.error(err));
+}
+getBusses();
+
+function updateBusses() {
+    let o = document.getElementsByClassName('busObj');
+    fetch('/getbus')
+    .then(response => { 
+        if(response.ok) {
+            return response.json(); // not important
+        }
+        }).then(data => {
+        if(data) { // if there is data
+            let i = 0;
+            while(i < data.buslist.length) {
+                let div = o[i];
+                getColor();
+                i++;
+
+                function getColor() {
+                    if (data.buslist[i].status == "Not Arrived") div.style.backgroundColor = "rgb(255, 44, 44)";
+                    else if (data.buslist[i].status == "Arrived") div.style.backgroundColor = 'green';
+                    else div.style.backgroundColor = 'grey';
+                }
+            }
+        }
+    }).catch(err => console.error(err));
 }
 
 function resize() {
