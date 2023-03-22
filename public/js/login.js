c339e0f66998a52f50061c6f5e303dfb746214ad
arremiasz <arremiasz@stu.naperville203.org>
2023-03-22 12:03:16 -0500

    working on creating login page

---
+++ public/js/login.js
@@ -0,0 +1,12 @@
+function resize() {
+    var w = window.innerWidth;
+    var h = window.innerHeight;
+
+    let loginbtn = document.getElementById('submitfield');
+    let loginbox = document.getElementById('login-box');
+    console.log(loginbox.style.width)
+
+
+    }
+resize();
+window.addEventListener("resize", resize);
