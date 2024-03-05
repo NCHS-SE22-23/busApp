b7eff1391e918bdd7a42a23197ac7e64fc99ea9f
arremiasz <arremiasz@stu.naperville203.org>
2024-03-05 13:30:51 -0600

    Added references to bash files

--- .platform/confighooks/prebuild/00_npm_install.sh
+++ .platform/confighooks/prebuild/00_npm_install.sh
@@ -2,3 +2,7 @@
 cd /var/app/staging
 sudo -u webapp mkdir node_modules
 sudo -u webapp npm install --omit=dev
+
+# References for Fix
+# https://stackoverflow.com/questions/71589818/why-npm-install-failed-only-in-elasticbeanstalk/74852276#74852276
+# https://github.com/lovell/sharp/issues/3221
